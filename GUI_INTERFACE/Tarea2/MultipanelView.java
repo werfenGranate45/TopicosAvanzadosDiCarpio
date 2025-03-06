02187db1bc1b4cbb9341103908a9c116216e9bfc
werfenGranate45 <eduardo.agustin.cervantes@gmail.com>
2025-03-06 00:28:03 -0600

    [UPDATE] Agrego clase anonima a la vista.

--- GUI_INTERFACE/Tarea2/MultipanelView.java
+++ GUI_INTERFACE/Tarea2/MultipanelView.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
+import java.awt.TextArea;
+
 //Para crear el paquete se debe tener el mismo nombre que la carpeta donde se aloja
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * Como va tener la ventana los componentes tendra
@@ -42,6 +45,7 @@ public class MultipanelView {
     }
 
     //Hacer rehuso con parametros para que la vista solo cambie
+    //Configuraremos el panel principal del menu
     private void setting(){
         window.setSize(1000, 600);
         window.setTitle("Ventana multipanel");
@@ -62,10 +66,10 @@ public class MultipanelView {
         
         //Configuracion de todas las imagenes asi bien insanas, bueno son de las etiquetas del container pricipal
         //Ojo para las etiquetas debes de poner un setBounds, que digamoslo levanta la imagen
-        imageOfContainer.setIcon(this.escaledImage("RecursoLeft4Dead\\Images_use\\HomeroUchiha.jpeg", 500, 200));
+        imageOfContainer.setIcon(this.escaledImage("GUI_INTERFACE\\Tarea2\\RecursoLeft4Dead\\Images_use\\HomeroUchiha.jpeg", 500, 200));
         imageOfContainer.setBounds(0, 0, 500, 200);
-        imageToButtonShut.setIcon(this.escaledImage("C:\\Users\\agust\\Documents\\TheCarpios\\GUI_INTERFACE\\Tarea2\\RecurosoPrograma\\Otro.jpg", 100, 60));
-        imageToButtonShut.setBounds(0,0,100,60);
+        imageToButtonShut.setText("Boton Apagado.");
+        imageToButtonShut.setBounds(0,0,50,50);
        
         //Comfiguras de los botones, en esta parte le ponemos las imagenes y los colocamos dentro del panel que va al sur
         buttonToRight.setBounds(850, 25, 50, 50);
@@ -77,7 +81,6 @@ public class MultipanelView {
     }
 
     private void assamble(){
-        
         container.add(imageOfContainer);
         buttonToRight.add(imageToButtonRigth);
         buttonToLeft.add(imageToButtonLeft);
@@ -96,5 +99,45 @@ public class MultipanelView {
         this.setting();
         this.assamble();
         this.display();
+    }
+
+    class SubPanelManage{
+        private JLabel image;
+        private JPanel container;
+        private JTextArea infoOfThing;
+        private MultipanelView mpv;
+
+        public void setImage(JLabel image){ this.image = image; }
+        public void setMainView(MultipanelView mpv){ this.mpv = mpv;}
+        public void setContainer(JPanel panel){ this.container = panel; }
+        public void setTextArea(JTextArea textArea){ this.infoOfThing = textArea; }
+
+        public JPanel getContainer(){ return this.container; }
+        public JTextArea getTextArea(){ return this.infoOfThing; }
+        public MultipanelView getViewMain(){ return this.mpv; }
+
+        private void assemblePanel(){
+            container.setSize(500,300);
+            container.setBounds(250, 200, 500, 300);
+            //Colocar imagen en el contendor
+            infoOfThing.setBounds(400, 100, 500, 300);
+            infoOfThing.setText("Hola aqui quiza va texto");
+            infoOfThing.setToolTipText("Hola va texto");
+        }
+
+        private void joinComponent(){
+            window.add(menu);
+            window.add(this.container);
+            window.add(this.infoOfThing);
+        }
+
+        private void noShow(){
+            window.setVisible(false);
+        }
+
+        private void show(){
+            window.setVisible(false);
+        }
+
     }
 }
