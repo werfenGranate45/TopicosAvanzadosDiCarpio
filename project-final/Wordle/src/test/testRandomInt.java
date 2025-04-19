51b23d75f14bb5f124df2db5e53d30caae1d0d75
werfenGranate45 <eduardo.agustin.cervantes@gmail.com>
2025-04-18 19:09:03 -0600

    El managerDB esta mejorado y se soluciona los bugs

---
+++ project-final/Wordle/src/test/testRandomInt.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Random;
+
+public class testRandomInt {
+    public static void main(String[] args) {
+        System.out.println(new Random().nextInt(1,5+1));
+        System.out.println(new Random().nextInt(1,5+1));
+        System.out.println(new Random().nextInt(1,5+1));
+        System.out.println(new Random().nextInt(1,5+1));
+        System.out.println(new Random().nextInt(1,5+1));
+        System.out.println(new Random().nextInt(1,5+1));
+        System.out.println(new Random().nextInt(1,5+1));
+        System.out.println(new Random().nextInt(1,5+1));
+        
+    }
+}
